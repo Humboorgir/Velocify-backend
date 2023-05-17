024b9fc2ecc23bcaf392ac648c921572e84e5e57
Humboorgir <hardcraftap@gmail.com>
2023-05-17 21:22:51 +0330

    installed typescript

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const port = 8080;
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.send("ttrgest");
+});
+app.listen(port, () => {
+    console.log("server running");
+});
+//# sourceMappingURL=app.js.map
