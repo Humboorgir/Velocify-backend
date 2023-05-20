505c43c4fecfc427193eaf1b5831d3fd9a89b937
Humboorgir <hardcraftap@gmail.com>
2023-05-20 13:40:46 +0330

    implement refresh tokens

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import userModel from "../models/user";
-import jwt from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "secret";
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || "rsecret";
 const router = express.Router();
+// TODO: implment this using a database
+const refreshTokens: string[] = [];
 // handling post requests sent to /auth/register
 router.post("/register", (req: Request, res: Response) => {
   // for testing purposes:
@@ -19,11 +22,31 @@ router.post("/register", (req: Request, res: Response) => {
     ok: true,
   });
 });
+
+// used for generating a new access token
+router.post("/token", (req: Request, res: Response) => {
+  let { token } = req.body;
+  if (!token) return res.sendStatus(401);
+  if (!refreshTokens.includes(token)) return res.sendStatus(403);
+
+  return jwt.verify(
+    token,
+    REFRESH_TOKEN_SECRET as Secret,
+    (err: any, user: any) => {
+      if (err) return res.sendStatus(403);
+      const { email, password } = user;
+      let accessToken = jwt.sign({ email, password }, ACCESS_TOKEN_SECRET);
+      return res.json({ accessToken });
+    }
+  );
+});
 router.post("/login", (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = { email, password };
-  const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
-  res.json({ accessToken });
+  let accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: "15s" });
+  let refreshToken = jwt.sign(user, REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken, refreshToken });
 });
 
 export default router;
