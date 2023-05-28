e373c6c7fddb546e63eff5b1f08f5d6f6e8972b1
Humboorgir <hardcraftap@gmail.com>
2023-05-28 14:02:32 +0330

    move routes loader into its own file

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@
 import { config } from "dotenv";
 config();
 
-import * as path from "path";
-import * as fs from "fs";
 import chalk from "chalk";
 
 import express, { Request, Response, Express } from "express";
@@ -13,6 +11,7 @@ import mongoose from "mongoose";
 
 import mongoConnect from "./utils/mongoconnect";
 import loadMongoEvents from "./utils/mongoEvents";
+import loadRouteFiles from "./utils/loadRouteFiles";
 
 const FRONTEND_SERVER = process.env.FRONTEND_SERVER || "http://localhost:3000";
 const port = process.env.PORT || 2000;
@@ -37,16 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // loading routes
-const routes: string[] = [];
-const routesPath = path.join(__dirname, "routes");
-const routeFiles = fs.readdirSync(routesPath);
-for (const route of routeFiles) {
-  // Removes the ".ts" at the end of the filenames.
-  const Route: string = `/${String(route).slice(0, -3)}`;
-  routes.push(Route);
-  // .default is neccessary here when using esm modules.
-  app.use(Route, require(`./routes/${Route}`).default);
-}
+loadRouteFiles(app);
 // return 404 if none of the defined routes match the url
 app.use((req: Request, res: Response) => {
   return res.status(404).send("Not found");
---
+++ src/utils/loadRouteFiles.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+import * as fs from "fs";
+import { Express } from "express";
+
+export default function loadRouteFiles(app: Express) {
+  const routesPath = path.join(__dirname, "..", "routes");
+  const routeFiles = fs.readdirSync(routesPath);
+  for (const route of routeFiles) {
+    // Removes the ".ts" at the end of the filenames.
+    const Route: string = `/${String(route).slice(0, -3)}`;
+    // .default is neccessary here when using esm modules.
+    app.use(Route, require(`../routes/${Route}`).default);
+  }
+}
