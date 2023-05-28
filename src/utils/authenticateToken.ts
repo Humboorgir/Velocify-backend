2f0a58859fea8ceff46e5827a66efb7e30dbbf96
Humboorgir <hardcraftap@gmail.com>
2023-05-28 14:18:00 +0330

    organize utils/authenticateToken

--- src/utils/authenticateToken.ts
+++ src/utils/authenticateToken.ts
@@ -14,11 +14,17 @@ export default function authenticateToken(
   res: Response,
   next: NextFunction
 ): any {
+  // get the authorization header
   const authHeader = req.headers["authorization"];
+  // remove 'Bearer' from the authorization header
+  // the authorization header consists of two parts: Bearer and the token
   const token = authHeader?.split(" ")[1];
+  // if there's no token return 401
   if (!token) return res.sendStatus(401);
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as Secret, (err, user) => {
+    // if the token is expired or invalid return 403
     if (err || !user) return res.sendStatus(403);
+
     const authenticatedReq = req as authenticatedRequest;
     authenticatedReq.user = user;
     return next();
