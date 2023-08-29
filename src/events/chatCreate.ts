5c940f5b0f3c0364659177a9732de6d1584cd0f7
Humboorgir <hardcraftap@gmail.com>
2023-08-29 23:44:15 +0330

    Implement callback for chatCreate event handler

--- src/events/chatCreate.ts
+++ src/events/chatCreate.ts
@@ -8,7 +8,6 @@ import { Server, Socket } from "socket.io";
 import jwt from "jsonwebtoken";
 import userModel from "../models/user";
 import chatModel from "../models/chat";
-import { Document } from "mongoose";
 interface IData {
   token: string;
   userId: string;
@@ -17,38 +16,47 @@ interface IO extends Server {
   IDs: Map<string, string>;
 }
 
-interface message {
-  author: Document;
-  content: string;
+interface chat {
+  _id: string;
+  participants: any[];
 }
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "secret";
 
-export default async function handler(
-  io: IO,
-  socket: Socket,
-  data: IData,
-  callback: (message: message) => void
-) {
+export default async function handler(io: IO, socket: Socket, data: IData, callback: (chat: chat) => void) {
   // note: userId contains the ID of the user we're sending a message to
   const { token, userId } = data;
   // authorize the user
-  if (!token) return;
+  if (!token) return console.log("token wasnt provided");
   try {
     // decode the token
     const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
     // if the token is expired or invalid:
-    if (typeof decoded === "string") return;
+    if (typeof decoded === "string") return console.log("Unauthorized");
     const user = await userModel.findOne({ _id: decoded._id });
     const otherUser = await userModel.findOne({ _id: userId });
-    if (!otherUser) return;
-    if (!user) return;
+    if (!otherUser) return console.log("request senders account wasnt found");
+    if (!user) return console.log("other users account wasnt found");
     const chat = new chatModel({
       participants: [user._id, otherUser._id],
       messages: [],
     });
-    chat.save();
+    const savedChat = await chat.save();
     (user.chats as any[]).push(chat._id);
     user.save();
+
+    callback({
+      _id: savedChat._id,
+      participants: [
+        {
+          _id: user._id,
+          username: user.username,
+        },
+        {
+          _id: otherUser._id,
+          username: otherUser.username,
+        },
+      ],
+    });
   } catch {
     // if the token is invalid or expired
     return;
