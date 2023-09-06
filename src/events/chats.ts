0ef8cc039ae93217803f12776e477e1bfb308b80
Humboorgir <hardcraftap@gmail.com>
2023-09-06 21:55:05 +0330

    Replace /chats api with socketio 'chats' event handler

---
+++ src/events/chats.ts
@@ -0,0 +1,28 @@
+import { Server, Socket } from "socket.io";
+import userModel from "../models/user";
+
+export default async function handler(io: Server, socket: Socket, data: any, callback: (chats: any) => void) {
+  const { decoded } = socket as any;
+  const userId = decoded._id;
+  if (userId != data) return console.log("User ids dont match");
+
+  let user = (
+    await userModel
+      .find({
+        _id: data,
+      })
+      .select("chats")
+      .populate({
+        path: "chats",
+        select: "participants",
+        populate: {
+          path: "participants",
+          select: "username",
+        },
+      })
+  )[0];
+
+  if (!user) return console.log("User wasnt found");
+
+  callback(user.chats);
+}
